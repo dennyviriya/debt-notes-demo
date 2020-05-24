25d7f42c57660c7e1663a10bbf7e3312fe5b0cd3
denny <denny.viriya@gmail.com>
2020-05-24 17:39:48 +0700

    delete all shit

--- src/main/kotlin/domain/entity/Debitur.kt
+++
@@ -1,8 +0,0 @@
-package domain.entity
-
-class Debitur(
-    var id: Long? = null,
-    var name: String,
-    var phoneNumber: String,
-    var address: String
-)
--- src/main/kotlin/domain/entity/Kreditur.kt
+++
@@ -1,6 +0,0 @@
-package domain.entity
-
-class Kreditur(
-    var id: Long,
-    var name: String
-)
